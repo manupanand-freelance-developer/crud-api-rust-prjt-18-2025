570263fc89204a1c0283cc3696d34eb8d5b265ed
manupanand <manupanandindianinvestor@gmail.com>
2025-06-11 21:05:54 +0530

    updated code

--- crud-api/src/main.rs
+++ crud-api/src/main.rs
@@ -1,5 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{http::header, HttpServer};
+use sqlx::{PgPool,Postgres,Pool,postgres::PgPoolOptions};
+use dotenv::dotenv;
+
+
+#[actix_web::main]
+
+async fn main()-> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none(){
+        std::env::set_var("RUST_LOG","actix_web=info");
+    }
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool: Pool<Postgres> = match PgPoolOptions::new()
+    .max_connections(10)
+    .connect(&database_url)
+    .await
+    {   // like try catch in js
+        Ok(pool) => { 
+            println!("Connection to db is successful");
+            pool  // no semicolon here, so `pool` is returned
+        }
+        Err(err) => {
+            println!("Failed to connect to the database {:?}", err);
+            std::process::exit(1);
+        }
+    };
+    println!("Server started succesfully")
+    HttpServer::new(move||{
+        let cors =Cors::default()
+        .allowed_origin("http:://localhost::3000")
+    })
 }
 /*
 - setup database 
